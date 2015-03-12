4ea6b417107b2356d434c95552bb1f419102556d
Logan Campbell <colin@logaan.net>
2015-03-13 04:03:00 +1100

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to blocking-js
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/blocking_js/core_test.clj
@@ -0,0 +1,7 @@
+(ns blocking-js.core-test
+  (:require [clojure.test :refer :all]
+            [blocking-js.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

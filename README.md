a184589ff532d3570abb685c4faa362e591e51a5
Logan Campbell <colin@logaan.net>
2015-03-13 07:08:46 +1100

    updated readme and project.clj with description

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # blocking-js
 
-A Clojure library designed to ... well, that part is up to you.
+A javascript transpiler that aims to allow programs to be written
+synchronously.
 
-## Usage
-
-FIXME
 
 ## License
 
-Copyright © 2015 FIXME
+Copyright © 2015 Logan Campbell
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject blocking-js "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A javascript transpiler that aims to allow programs to be
+               written synchronously."
+  :url "https://github.com/logaan/blocking-js"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]

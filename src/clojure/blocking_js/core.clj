7045199c9c5f43b80ddace12521dcea5973e516a
Logan Campbell <colin@logaan.net>
2015-03-13 05:49:16 +1100

    transforming antlr tree into clojure map tree

--- src/clojure/blocking_js/core.clj
+++ src/clojure/blocking_js/core.clj
@@ -1,16 +1,19 @@
 (ns blocking-js.core
-  (:import [org.antlr.v4.runtime ANTLRFileStream CommonTokenStream]))
+  (:require [clojure.reflect :as r])
+  (:import [org.antlr.v4.runtime ANTLRFileStream CommonTokenStream]
+           [clojure.lang Reflector]))
 
+(defn find-branch-method-names [context]
+  (->> context
+       r/reflect
+       :members
+       (filter (fn [m] (empty? (:parameter-types m))))
+       (map :name)
+       (map str)
+       (filter (fn [m] (not (= "getRuleIndex" m))))))
 
-; ANTLRInputStream input = new ANTLRFileStream(args[0]);
-; RLexer lexer = new RLexer(input);
-; CommonTokenStream tokens = new CommonTokenStream(lexer);
-; RParser parser = new RParser(tokens);
-; parser.setBuildParseTree(true);
-; RuleContext tree = parser.prog();
-; tree.inspect(parser); // show in gui
-; //tree.save(parser, "/tmp/R.ps"); // Generate postscript
-; System.out.println(tree.toStringTree(parser));
+(defn call-method [object method-name]
+  (Reflector/invokeInstanceMethod object method-name (to-array [])))
 
 (defn restart []
   (let [parser (-> (ANTLRFileStream. "resources/hiWaitBye.js")
@@ -18,9 +21,23 @@
                    CommonTokenStream.
                    ECMAScriptParser.)]
     (.setBuildParseTree parser true)
-    (let [tree (.program parser)]
-      (.inspect tree parser)
-      (.println System/out (.toStringTree tree parser)))))
+    (.program parser)))
 
-(restart)
+(defn do-mapify [context]
+  (let [info {:text (.getText context)
+              :type (type context)}]
+    (if (zero? (.getChildCount context))
+      info
+      (->> (find-branch-method-names context)
+           (map #(vector (keyword %) (mapify (call-method context %))))
+           (into {})
+           (merge info)))))
+
+(defn mapify [context]
+  (if context
+    (if (= java.util.ArrayList (class context))
+      (map do-mapify context)
+      (do-mapify context))))
+
+#spy/p (mapify (restart))
 

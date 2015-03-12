cd3a9541679412a771c6c377e2ccceea01fbf11e
Logan Campbell <colin@logaan.net>
2015-03-13 07:05:17 +1100

    maintain child order

--- src/clojure/blocking_js/core.clj
+++ src/clojure/blocking_js/core.clj
@@ -4,9 +4,6 @@
   (:import [org.antlr.v4.runtime ANTLRFileStream CommonTokenStream ParserRuleContext]
            [clojure.lang Reflector]))
 
-; Might be easier to implement in terms of .children
-; Doesn't maintain order of child nodes
-
 (defn find-branch-method-names [context]
   (->> context
        r/reflect
@@ -26,24 +23,23 @@
 
 (defn sorted-map-from-value-list [unsorted-map value-list]
   (let [inverted (s/map-invert unsorted-map)]
-    (reduce (fn [acc value]
-              (let [key (inverted value)]
-                (if (nil? key) #spy/p value)
-                (assoc acc key  value)))
-            (sorted-map) value-list)))
+    (-> (reduce (fn [acc value] (assoc acc (inverted value) value))
+                (sorted-map) value-list)
+        (dissoc nil))))
 
 (defn map-vals [t m]
   (into (empty m) (map (fn [[k v]] [k (t v)]) m)))
 
 (defn with-children [context]
   (if (zero? (.getChildCount context))
-    {:object context}
+    {:object context
+     :text   (.getText context)}
     {:object context 
-     :children (map-vals
-                 with-children
-                 (sorted-map-from-value-list
-                   (do-mapify context)
-                   (.children context)))}))
+     :children (let [sorted (sorted-map-from-value-list (do-mapify context)
+                                                        (.children context))]
+                 (if (empty? sorted)
+                   (map with-children (.children context))     
+                   (map-vals with-children sorted)))}))
 
 (defn main []
   (let [parser (-> (ANTLRFileStream. "resources/hiWaitBye.js")
@@ -53,5 +49,9 @@
     (.setBuildParseTree parser true)
     (.program parser)))
 
-(with-children (main))
+(comment 
+
+  (with-children (main))
+  
+  )
 
